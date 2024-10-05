c8a132cf08a36c5fec8d22520bc1ba1bb050f3a4
Hyejeong Son <154120089+sonhj0125@users.noreply.github.com>
2024-10-05 13:52:15 +0900

    Title: 숫자 2개 출력, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241005/숫자 2개 출력/print-two-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        // System.out.print("3 5");
+        // 또는
+        System.out.print(3 + " " + 5);
+    }
+}
