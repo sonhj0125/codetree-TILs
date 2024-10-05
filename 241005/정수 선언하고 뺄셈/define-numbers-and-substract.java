ea2bb5fc2a0b48e0bb6695d7189b8d326a6862f8
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-05 22:56:00 +0900

    Title: 정수 선언하고 뺄셈, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241005/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 97;
+        int b = 13;
+
+        System.out.println(a + " - " + b + " = " + (a-b));
+
+    }
+}
