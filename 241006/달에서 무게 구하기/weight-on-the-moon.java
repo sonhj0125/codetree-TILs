88a85565af8778da303db52aa3548f5a1340c3a0
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-06 17:55:49 +0900

    Title: 달에서 무게 구하기, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241006/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a = 13;
+        double b = 0.165;
+
+        // 정수 * 6째자리 소수 = 6째자리 소수
+        System.out.printf("%d * %.6f = %.6f", a, b, a * b);
+    }
+}
