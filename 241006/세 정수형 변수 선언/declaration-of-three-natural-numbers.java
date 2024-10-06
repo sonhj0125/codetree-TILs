928c098dee940ca72adff14c6ec8efced7ea5532
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-06 17:18:03 +0900

    Title: 세 정수형 변수 선언, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241006/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a = 7;
+        int b = 23;
+        int c = 30;
+
+        System.out.println(a + " + " + b + " = " + c);
+
+    }
+}
