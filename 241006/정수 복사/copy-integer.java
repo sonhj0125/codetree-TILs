a304777558a368fea420ed83849284db619edade
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-06 18:41:13 +0900

    Title: 정수 복사, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241006/정수 복사/copy-integer.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a = 3;
+        int b = 4;
+        b = a;
+
+        System.out.println(a + " " + b);
+        System.out.println(a*b);
+    }
+}
