60359a9fbf77c92734a977e7e4cc8ec3b079d5a8
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-06 17:25:35 +0900

    Title: 변수 출력하기 3, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241006/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a = 1;
+        int b = 2;
+        char c = 'C';
+
+        System.out.print(a + "->" + b + "->" + c);
+    }
+}
