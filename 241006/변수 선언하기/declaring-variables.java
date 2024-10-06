4cbb903af8480077b97631823fe64f37ec22a061
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-06 17:08:10 +0900

    Title: 변수 선언하기, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241006/변수 선언하기/declaring-variables.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 3;
+        char b = 'C';
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
