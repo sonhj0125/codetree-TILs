d2f4e1ba5df79203b37aa20dd97a59a20bde149f
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-07 23:34:25 +0900

    Title: 합을 복사하기, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241007/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a = 1, b = 2, c = 3;
+
+        a = b = c = (a+b+c);
+
+        System.out.println(a + " " + b + " " + c);
+    }
+}
