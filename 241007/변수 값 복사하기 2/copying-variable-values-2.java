1320f177352f580210ec1b26efba08c8c2cceb0d
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-07 23:32:46 +0900

    Title: 변수 값 복사하기 2, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241007/변수 값 복사하기 2/copying-variable-values-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a = 5, b = 6, c = 7;
+
+        a = b = c;
+
+        System.out.println(a + " " + b + " " + c);
+    }
+}
