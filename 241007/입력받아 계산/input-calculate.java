1acd2c073a6d0edb763e56096b2c696df1e05b2f
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-07 23:36:21 +0900

    Title: 입력받아 계산, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241007/입력받아 계산/input-calculate.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        sc.close();
+
+        System.out.println(a+2);
+    }
+}
