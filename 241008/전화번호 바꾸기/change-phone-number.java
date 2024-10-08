caf8b9f1900670eb48828f519ec9fce806d7e11d
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-08 17:43:03 +0900

    Title: 전화번호 바꾸기, Time: 216ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241008/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+
+        String a = sc.next();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
+
+        System.out.print(a + "-" + c + "-" + b);
+
+    
+    
+    }
+
+}
