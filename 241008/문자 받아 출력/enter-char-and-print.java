3a726ed2437906ebf5529be4a1f9126e6b5f6f47
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-08 17:16:43 +0900

    Title: 문자 받아 출력, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241008/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+
+        System.out.println(c);
+    }
+}
