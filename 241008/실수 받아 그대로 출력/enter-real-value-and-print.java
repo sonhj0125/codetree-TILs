3957741031d14d94f3c157447c36bcaca6b2fb15
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-08 14:54:41 +0900

    Title: 실수 받아 그대로 출력, Time: 154ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241008/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        double n = sc.nextDouble();
+        sc.close();
+
+        System.out.printf("%.2f", n);
+
+
+    }
+}
