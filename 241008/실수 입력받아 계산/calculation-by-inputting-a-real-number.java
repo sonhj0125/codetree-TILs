d5d6120ba000c5ddc2eab6fdbe031e02a647a163
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-08 17:08:34 +0900

    Title: 실수 입력받아 계산, Time: 150ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241008/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        sc.close();
+
+        System.out.printf("%.2f", a+b);
+        
+    }
+}
