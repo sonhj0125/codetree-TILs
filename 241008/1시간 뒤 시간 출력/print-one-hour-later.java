a4813e944bd9a6e0b91b461f17208ff15294b361
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-08 17:29:23 +0900

    Title: 1시간 뒤 시간 출력, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241008/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+    
+        sc.useDelimiter(":");
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        sc.close();
+
+        System.out.println(h+1 + ":" + m);
+    
+    }
+}
