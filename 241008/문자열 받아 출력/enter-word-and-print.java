a01c9071f888ed2f87ad8e6847da95d9666e0422
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-08 17:20:08 +0900

    Title: 문자열 받아 출력, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241008/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        sc.close();
+
+        System.out.print(s);
+    }
+}
