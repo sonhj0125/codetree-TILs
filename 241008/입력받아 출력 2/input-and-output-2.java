9d07267bef0a7fb8e1072c4279f59f2f7795fbe8
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-08 17:34:47 +0900

    Title: 입력받아 출력 2, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241008/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+
+        int a,b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.print(a);
+        System.out.print(b);
+
+    }
+}
