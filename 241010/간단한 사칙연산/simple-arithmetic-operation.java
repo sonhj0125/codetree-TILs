b292dc4a5b0122124f24e28cf455fa805b5e6c0f
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-10 19:28:31 +0900

    Title: 간단한 사칙연산, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241010/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b); // 몫
+        System.out.println(a%b); // 나머지
+
+
+    }
+}
