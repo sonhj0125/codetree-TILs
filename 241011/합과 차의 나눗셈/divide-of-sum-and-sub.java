d2ffb8905c865dc4850bb429a7ad8e9068439b76
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-11 17:42:39 +0900

    Title: 합과 차의 나눗셈, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241011/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("%.2f", (double)(a+b)/(a-b));
+
+    }
+}
