f6bc4a466e3e4086c7d6f7bc813044034dbe6ee0
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-14 17:55:36 +0900

    Title: 출석 부르기, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241014/출석 부르기/calling-attendance.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+    
+        int num = sc.nextInt();
+        sc.close();
+
+        if(num == 1){
+            System.out.println("John");
+        }
+        else if(num == 2){
+            System.out.println("Tom");
+        }
+        else if(num == 3){
+            System.out.println("Paul");
+        }
+    
+    
+    
+    
+    
+    
+    }
+
+}
