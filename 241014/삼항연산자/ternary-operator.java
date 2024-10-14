19f7e6a07bc576e32d00619a1c65d4ea1c862d39
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-14 17:12:49 +0900

    Title: 삼항연산자, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241014/삼항연산자/ternary-operator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        sc.close();
+
+        System.out.println(num == 100 ? "pass" : "failure");
+
+
+    }
+}
