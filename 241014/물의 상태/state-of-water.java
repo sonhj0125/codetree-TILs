9181b75218b4950fdb2e638c7d40766110ec3e9c
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-14 17:29:43 +0900

    Title: 물의 상태, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241014/물의 상태/state-of-water.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+    
+        int temp = sc.nextInt();
+        sc.close();
+
+        if(temp < 0){
+            System.out.println("ice");
+        }
+        else if(temp >= 100){
+            System.out.println("vapor");
+        }
+        else if(0 <= temp && temp < 100){
+            System.out.println("water");
+        }
+
+    
+    
+    }
+
+}
