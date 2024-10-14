fa56971833179d874cdf10265a51ba4eff7c892b
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-14 18:17:57 +0900

    Title: 살 수 있는 물건 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241014/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.close();
+
+        if(n >= 3000){
+            System.out.println("book");
+        }
+        else if(n >= 1000){
+            System.out.println("mask");
+        }
+        else if(n >= 500){
+            System.out.println("pen");
+        }
+        else {
+            System.out.println("no");
+        }
+    }
+}
