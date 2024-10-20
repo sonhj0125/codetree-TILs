1038f45a2bd664bfb8e9e7597326209b7f08c9a0
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-20 17:24:42 +0900

    Title: 비교 연산, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241020/비교 연산/comparison-operator.java
+++ 241020/비교 연산/comparison-operator.java
@@ -12,48 +12,12 @@ public class Main {
         b = sc.nextInt();
         sc.close();
 
-        if(a >= b) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
-
-        if(a > b) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
-
-        if(b >= a) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
-        
-        if(b > a) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
-
-        if(a == b) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
-        
-        if(a != b){
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        
-        }
+        System.out.println(a >= b ? 1 :0);
+        System.out.println(a > b ? 1 :0);
+        System.out.println(a <= b ? 1 :0);
+        System.out.println(a < b ? 1 :0);
+        System.out.println(a == b ? 1 :0);
+        System.out.println(a != b ? 1 :0);
 
 
 
