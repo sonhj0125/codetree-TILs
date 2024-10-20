faa82ef1aaec90170bea4999aff834c15a67a547
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-20 17:27:04 +0900

    Title: 4가지 관계연산자, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241020/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;   
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sc.close();
+
+        System.out.println(a >= b ? 1 : 0);
+        System.out.println(a > b ? 1 : 0);
+        System.out.println(a <= b ? 1 : 0);
+        System.out.println(a < b ? 1 : 0);
+
+    }
+}
