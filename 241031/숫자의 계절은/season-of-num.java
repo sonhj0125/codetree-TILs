990cc017cc89698c10bd633094880690a77540f8
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-10-31 17:11:26 +0900

    Title: 숫자의 계절은, Time: 227ms, Memory: 10MB, Status: Passed - CodeTree

--- 241031/숫자의 계절은/season-of-num.java
+++ 241031/숫자의 계절은/season-of-num.java
@@ -8,13 +8,13 @@ public class Main {
 
         int m = sc.nextInt();
 
-        if(m == 12 || m <= 2 && m != 0) {
+        if(m >= 12 || m <= 2) {
             System.out.println("Winter");
         }
-        else if(m >= 3 && m <= 5) {
+        else if(m <= 5) {
             System.out.println("Spring");
         }
-        else if(m >= 6 && m <= 8) {
+        else if(m <= 8) {
             System.out.println("Summer");
         }
         else {
