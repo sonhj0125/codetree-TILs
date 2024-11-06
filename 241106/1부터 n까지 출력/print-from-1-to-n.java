ac01bd5b7f94d601d54711879f7886a69cab4788
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-11-06 18:09:41 +0900

    Title: 1부터 n까지 출력, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241106/1부터 n까지 출력/print-from-1-to-n.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        for(int i=1; i<=n; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
