f554746040a5be13a1a24d343e482185e52516fd
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-11-06 17:42:18 +0900

    Title: 차례로 출력, Time: 76ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241106/차례로 출력/print-in-order.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        
+        for(int i=5; i<18; i++){
+            System.out.print(i + " ");
+        }
+        
+
+    }
+}
