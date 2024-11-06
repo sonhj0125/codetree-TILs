62766b409b0ecd1a0c758c80e850fb6b9747fab3
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-11-06 15:43:24 +0900

    Title: 윤년인가, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241106/윤년인가/is-leap-year.java
+++ 241106/윤년인가/is-leap-year.java
@@ -12,10 +12,7 @@ public class Main {
         // 4로 나누어 떨어지는 해는 윤년, 그 밖의 해는 평년
         // 단, 예외적으로 100으로 나누어 떨어지되, 400으로 나누어 떨어지지 않는 해는 평년
 
-        if(y % 4 == 0) {
-            System.out.println("true");
-        }
-        else if(y % 100 == 0 && y % 400 != 0) {
+        if((y % 4 == 0 && y % 100 == 0) || (y % 400 != 0)) {
             System.out.println("true");
         }
         else {
