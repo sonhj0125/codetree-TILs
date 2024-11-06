67f7261071be8e45ea704f25d2c4feebcbe82157
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-11-06 19:27:54 +0900

    Title: 감소시키며 출력하기 2, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241106/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int i = 26;
+        while(i>=10){
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
