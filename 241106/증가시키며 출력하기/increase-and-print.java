89a1bde2217249c223d616d4a0c438632bee597a
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-11-06 18:12:09 +0900

    Title: 증가시키며 출력하기, Time: 79ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241106/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        for(int i=5; i<18; i+=2) {
+            System.out.print(i + " ");
+        }
+    }
+}
