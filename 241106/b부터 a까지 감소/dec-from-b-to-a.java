64330a40db561c191b46f78234781fc82969a2dc
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-11-06 19:02:19 +0900

    Title: b부터 a까지 감소, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241106/b부터 a까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i=b; i>=a; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
