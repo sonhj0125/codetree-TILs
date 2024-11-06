ba86183f877f5a1a2aca7c8f8441a1a7777f7a05
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-11-06 19:27:04 +0900

    Title: 짝수만 출력, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241106/짝수만 출력/output-only-even.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int i = a;
+        while(i<=b) {
+            System.out.print(i + " ");
+            i+=2;
+        }
+        
+
+    }
+}
