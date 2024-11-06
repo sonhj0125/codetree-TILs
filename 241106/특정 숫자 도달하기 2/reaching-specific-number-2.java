f73e6593d7bcc49bc3de384f631d6c0e6639e743
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-11-06 19:00:12 +0900

    Title: 특정 숫자 도달하기 2, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241106/특정 숫자 도달하기 2/reaching-specific-number-2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.close();
+
+        for(int i=n; i>=1; i--){
+            System.out.print(i + " ");
+        }
+    }
+}
