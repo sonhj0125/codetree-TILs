b0fd35ee071889198b20ca1ca6c4530e4a9f7057
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2024-11-15 18:12:55 +0900

    Title: 2 또는 3의 배수, Time: 109ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241115/2 또는 3의 배수/multiple-of-2-or-3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            if(i % 2 == 0 || i % 3 == 0) {
+                System.out.print(1 + " ");
+            }
+            else {
+                System.out.print(0 + " ");
+            }
+        }
+        
+    }
+}
